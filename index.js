9e43dc74dde57ddcfb0cd2a884e823fd5b46c316
dancefactory <youngkinam@yahoo.com>
2022-10-13 16:51:56 +0800

    port 80 again

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.get("/healthcheck", (req, res) => {
 require("./src/routes")(app);
 require("./src/routes/reuseCRUD")(app);
 
-const PORT = 8484;
+const PORT = 80;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
